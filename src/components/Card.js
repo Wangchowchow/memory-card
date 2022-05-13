4f5f57133f9f3ef57d377b675ebc367ff58b5e9d
Wangchowchow <wangsahioe@gmail.com>
2022-05-13 14:07:00 +0700

    add cards backbone

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Card(props) {
+    const {
+        card: { src, value },
+        handleGame
+    } = props;
+
+    return (
+        <button className="card" onClick={handleGame}>
+            <img src={src} alt={value}></img>
+            <p>{value}</p>
+        </button>
+    )
+}
+
+export default Card;
---
+++ src/components/CardContainer.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+
+import alaskanMalamute from "../assets/alaskanmalamute";
+import bulldog from "../assets/bulldog";
+import germanSheperd from "../assets/germansheperd";
+import goldenRetreiver from "../assets/goldenretreiver";
+import labradorRetreiver from "../assets/labradorretreiver";
+import pomeranian from "../assets/pomeranian";
+import pug from "../assets/pug";
+import shibaInu from "../assets/shibainu";
+import shihTzu from "../assets/shihtzu";
+import siberianHusky from "../assets/siberianhusky";
+
