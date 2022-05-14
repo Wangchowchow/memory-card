ae9468e78e421144b1d2272590253b99a8cd7052
Wangchowchow <wangsahioe@gmail.com>
2022-05-14 07:59:40 +0700

    finish preliminary web page design

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -12,3 +12,51 @@ import shibaInu from "../assets/shibainu";
 import shihTzu from "../assets/shihtzu";
 import siberianHusky from "../assets/siberianhusky";
 
+function CardContainer(props) {
+    const { handleGame, score, highScore } = props;
+
+    let cards = [
+        {
+            src: alaskanMalamute,
+            value: "Alaskan Malamute"
+        },
+        {
+            src: bulldog,
+            value: "Bulldog"
+        },
+        {
+            src: germanSheperd,
+            value: "German Sheperd"
+        },
+        {
+            src: goldenRetreiver,
+            value: "Golden Retreiver"
+        },
+        {
+            src: labradorRetreiver,
+            value: "Labrador Retreiver"
+        },
+        {
+            src: pomeranian,
+            value: "Pomeranian"
+        },
+        {
+            src: pug,
+            value: "Pug"
+        },
+        {
+            src: shibaInu,
+            value: "Shiba Inu"
+        },
+        {
+            src: shihTzu,
+            value: "Shih Tzu"
+        },
+        {
+            src: siberianHusky,
+            value: "Siberian Husky"
+        }
+    ];
+
+    const []
+}
